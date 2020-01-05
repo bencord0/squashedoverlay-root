5457bfb1ded95ed468839d543830e3506f577976
Ben Cordero <bencord0@condi.me>
2020-01-05 17:22:40 +0000

    Don't overwrite existing modules in sysroot
    The initramfs typically holds a subset of all installed kernel
    modules. Don't overwrite files, like modules.dep, that will
    break kmod tools if they don't include all of the available modules.

--- 81squashedoverlay-root/pre-pivot-squashedoverlay-root.sh
+++ 81squashedoverlay-root/pre-pivot-squashedoverlay-root.sh
@@ -1,2 +1,4 @@
-mkdir -p /sysroot/lib/modules
-cp -r /lib/modules/* /sysroot/lib/modules/
+if [[ -! -d "/sysroot/lib/modules/$(uname -r)" ]]; do
+    mkdir -p /sysroot/lib/modules
+    cp -r /lib/modules/* /sysroot/lib/modules/
+done
